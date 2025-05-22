7eda491e305d878b6ad6a5877432c6b1eb057d3b
leh <lf182000@gmail.com>
2025-05-22 14:08:29 -0300

    set timeout and multiply

--- stream/fundamentals.js
+++ stream/fundamentals.js
@@ -1,23 +1,30 @@
-// process.stdin
-//     .pipe(process.stdout)
-
-import { Readable } from "node:stream"
+import { Readable, Writable } from "node:stream"
 
 class OneToHundredStream extends Readable {
     index = 1
-    
+
     _read() {
         const i = this.index++
 
-        if(i > 100) {
-            this.push(null)
-        } else {
-            const buf = Buffer.from(String(i))
+        setTimeout(() => {
+            if (i > 100) {
+                this.push(null)
+            } else {
+                const buf = Buffer.from(String(i))
 
-            this.push(buf)
-        }
+                this.push(buf)
+            }
+        }, 1000)
+    }
+}
+
+class MultiplyByTenStream extends Writable {
+    _write(chunk, encoding, callback) {
+        console.log(Number(chunk.toString()) * 10)
+
+        callback()
     }
 }
 
 new OneToHundredStream()
-    .pipe(process.stdout)
+    .pipe(new MultiplyByTenStream())

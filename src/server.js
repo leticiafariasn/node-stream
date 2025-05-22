e0461f5f9af739a93524dff1d9e8cf5d525d7ec7
leh <lf182000@gmail.com>
2025-05-22 13:56:09 -0300

    init commit

---
+++ src/server.js
@@ -0,0 +1,26 @@
+import http from 'node:http'
+
+const users = []
+
+const server = http.createServer((req, res) => {
+    const {method, url} = req
+
+    if (method === "GET" && url === '/users') {
+        return res
+        .setHeader('Content-Type', 'application/json')
+    }
+
+    if(method === "POST" && url === '/users') {
+        users.push({
+            id: 1,
+            name: 'Letícia Farias',
+            email: 'leticiafarias@email.com'
+        })
+
+        return res.writeHead(201).end()
+    }
+
+    return res.writeHead(404).end()
+})
+
+server.listen(3333)

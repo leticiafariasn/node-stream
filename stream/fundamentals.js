ae2dda3856fef6434275344a64bbae4fefc25c56
leh <lf182000@gmail.com>
2025-05-22 14:30:28 -0300

    adding transform

--- stream/fundamentals.js
+++ stream/fundamentals.js
@@ -1,4 +1,4 @@
-import { Readable, Writable } from "node:stream"
+import { Readable, Transform, Writable } from "node:stream"
 
 class OneToHundredStream extends Readable {
     index = 1
@@ -18,6 +18,14 @@ class OneToHundredStream extends Readable {
     }
 }
 
+class NegativeNumberToStream extends Transform {
+    _transform(chunk, encoding, callback){
+        const transformed = Number(chunk.toString()) * -1
+
+        callback(null, Buffer.from(String(transformed))) 
+    }
+}
+
 class MultiplyByTenStream extends Writable {
     _write(chunk, encoding, callback) {
         console.log(Number(chunk.toString()) * 10)
@@ -27,4 +35,5 @@ class MultiplyByTenStream extends Writable {
 }
 
 new OneToHundredStream()
+    .pipe(new NegativeNumberToStream())
     .pipe(new MultiplyByTenStream())

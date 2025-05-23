0c2c8df516704f9ef8818ad13b5609dcc6754c4f
leh <lf182000@gmail.com>
2025-05-23 16:16:23 -0300

    att prof

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import http from 'node:http'
 
 const users = []
 
-const server = http.createServer(async(req, res) => {
+const server = http.createServer(async (req, res) => {
     const {method, url} = req
 
     const buffers = []

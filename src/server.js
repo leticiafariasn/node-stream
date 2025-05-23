56961dd7d7a0d1c380e01066bbd38e52f07e7cc4
leh <lf182000@gmail.com>
2025-05-23 16:08:53 -0300

    add async

--- src/server.js
+++ src/server.js
@@ -2,8 +2,17 @@ import http from 'node:http'
 
 const users = []
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async(req, res) => {
     const {method, url} = req
+
+    const buffers = []
+
+    for await (const chunk of req) {
+        buffers.push(chunk)
+    }
+
+    const body = Buffer.concat(buffers).toString()
+    console.log(body)
  
     if (method === "GET" && url ==='/users') {
         return res
